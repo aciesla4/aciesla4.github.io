c685ea9b34fcba9ad217cc2d6e6d4e58c1bdb23c
WilliamCollicott <31823020+WilliamCollicott@users.noreply.github.com>
2018-10-23 12:11:25 -0400

    Started handling login verification, changed page destination in createAccount
    There is now php code in checkLogin that will verify a user's information that they entered. If the information is corrent and a user exists with the matching credentials, the profile page is loaded. If there is no matching users, the page directs the user to create an arrount (does not direct them to the createAccount page). I also changed the destination of the createAccount page to be the profile page, instead of the newAccount page. After a user creates their account, we want to take them directly to their new profile page.

--- checkLogin.php
+++ checkLogin.php
@@ -1,1 +1,53 @@
-hi
+<?php>
+//Start the session
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php>
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+
+            $servername = "classdb.it.mtu.edu";
+            $dbUsername = "wads_rw";
+            $dbPassword = "wadsworth";
+            $dbName = "wads";
+
+            // Create connection
+            $conn = new mysqli ($servername, $dbUsername, $dbPassword, $dbName);
+
+            // Check connection
+            if ($conn->connect_eror) {
+                die("Connection failed: " . $conn->connect_error);
+            } else {
+                echo "Connection successful";
+            }
+
+            // Prepare and bind
+            $sql = "SELECT * FROM User WHERE userName = $username, password = $password";
+            $result = $conn->query($sql);
+
+            $row = $result->fetch_assoc();
+            if($result->num_rows == 1) {
+
+                // Session variables are saved so that in the profile.php page, the corrent user profile can be loaded
+                $_SESSION["theUserName"] = $row["userName"];
+                $_SESSION["thePassword"] = $row["password"];
+
+                // Go to that user's profile page
+                header("Location: profile.php");
+                
+                echo "Welcome, $row[name].";
+                
+            } else if($result->num_rows > 1) {
+                echo "There is more than one user with this information, what should we do?";
+            }
+            else if($result->num_rows == 0) {
+                // There are no users with those credentials
+                echo "There are no accounts matching that information; please create an account";
+            }
+        ?>
+    </body>
+</html>
