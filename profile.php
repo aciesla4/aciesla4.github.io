6539633698d4f2018f85e605725e36ad998de5d9
Derek Gregg <42588472+derekjgregg@users.noreply.github.com>
2018-10-24 12:27:14 -0400

    Update profile.php attempt

--- profile.php
+++ profile.php
@@ -1,4 +1,15 @@
 <!DOCTYPE html>
 <html>
-This is a test.
+   <body>
+      <?php
+         $username = $_GET['username1'];
+         $password = $_GET['password1'];
+         foreach($dbh->query("SELECT * FROM User WHERE userName = '$username' AND password = '$password'") as $row) {
+            $_SESSION["name"] = $name;
+         }
+         <p>
+            echo 'Welcome '.$name.'!';
+         </p>
+      ?>
+   </body>
 </html>
