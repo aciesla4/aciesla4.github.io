45e2a3ac91745d704825bc024d16c4c6384f62d4
Ann Ciesla <aeciesla@colossus.it.mtu.edu>
2018-10-24 21:06:30 -0400

    New index page

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1> Fantasy Broomball </h1>
+<p> Wlecome to Fantasy Broomball </p>
+<p> Click here to login: <br/> 
+<a href="aciesla4.github.io/login.php">Login</a><br/>
+</p>
+<p> Click here to create an Account: <br/>
+<a href="aciesla4.github.io/createAccount.php">Create Account</a><br/>
+</p>
+</body>
+</html>
