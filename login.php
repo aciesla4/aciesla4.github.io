6f8ae917408edb45a4b2e71565f669c75b590fe5
Ann Ciesla <aeciesla@colossus.it.mtu.edu>
2018-10-25 15:27:04 -0400

    Added email verification

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <body>
 	<h2>Login</h2>
 		<form action = "checkLogin.php" method = "post">
-			Username:<br/> <input type = "text" name = "username" required/> <br/>
+			Username:<br/> <input type = "email" placeholder = "email@example.com" name = "username" required/> <br/>
 			Password:<br/> <input type = "password" name = "password" required/> <br/>
 			<input type = "submit" name = "login" value = "Login"/>
 		</form>
