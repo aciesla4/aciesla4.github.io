d6546a52c43edd707e1d1213b99459bb960c1b1d
Ann Ciesla <aeciesla@colossus.it.mtu.edu>
2018-10-25 15:16:04 -0400

    Added link names

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 <h1> Fantasy Broomball </h1>
 <p> Wlecome to Fantasy Broomball </p>
 <p> Click here to login: <br/> 
-<a href="aciesla4.github.io/login.php">Login</a><br/>
+<a href="aciesla4.github.io/login.php" name="loginLink">Login</a><br/>
 </p>
 <p> Click here to create an Account: <br/>
-<a href="aciesla4.github.io/createAccount.php">Create Account</a><br/>
+<a href="aciesla4.github.io/createAccount.php" name="createAccountLink">Create Account</a><br/>
 </p>
 </body>
 </html>
