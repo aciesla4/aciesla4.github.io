8d33284b89cc2b81952922ab7aa2ccdc7a2460e5
Derek Gregg <42588472+derekjgregg@users.noreply.github.com>
2018-10-31 12:25:16 -0400

    Update createAccount.php

--- createAccount.php
+++ createAccount.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html>
-<body>
-	<h2>Create an Account</h2>
+	<body>
+		<h2>Create an Account</h2>
 		<form action = "newAccount.php" method = "post">
-			Name:<br/> <input type =  "text" name="name" required /><br/>
-			Username:<br/> <input type = "email" placeholder="email@example.com" name = "username" required /><br />
-			Password:<br/> <input type = "password" name = "password" required /><br/>
-			Team Name: <br/> <input type = "text" name = "teamName" required /><br/>
-			<br />
-			<input type = "submit" name = "createAccount" value = "Create Account"/>
+			<div style="margin-left: 1em;">
+				Name:<br/> <input type =  "text" name="name" required /><br/>
+				Username:<br/> <input type = "email" placeholder="email@example.com" name = "username" required /><br />
+				Password:<br/> <input type = "password" name = "password" required /><br/>
+				Team Name: <br/> <input type = "text" name = "teamName" required /><br/>
+				<br />
+				<input type = "submit" name = "createAccount" value = "Create Account"/>
+			</div>
 		</form>
-</body>
+	</body>
 </html>
